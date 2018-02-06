eedf9e72b02f29a9f03c62f2f1677458d68de6e2
Theo Salzmann <dev@on3iro.de>
2018-02-06 08:13:41 +0100

    Change errCallback to general callbackFn inside makeValidate

--- src/_tests_/makeValidate.test.js
+++ src/_tests_/makeValidate.test.js
@@ -2,7 +2,6 @@
 
 import makeValidate from '../makeValidate'
 import {
-  isString,
   isNumberString
 } from '../validators'
 
@@ -29,22 +28,7 @@ describe('makeValidate()', () => {
 describe('makeValidate() with callback', () => {
   const setUp = makeSetUp(makeValidate)
 
-  it('should not call callback if validation succeeds', () => {
-    const callbackMock = jest.fn()
-    const { result } = setUp({
-      fields: [
-        {
-          selector: '.input_num-string',
-          validators: [ isString, isNumberString ]
-        }
-      ]
-    }, callbackMock)
-
-    expect(result()).toBe(true)
-    expect(callbackMock).not.toHaveBeenCalled()
-  })
-
-  it('should call callback if validation fails', () => {
+  it('should invoke callback', () => {
     const callbackMock = jest.fn().mockReturnValue(false)
     const { result } = setUp({
       fields: [

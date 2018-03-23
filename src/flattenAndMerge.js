abbc22fa44baed23dbbee021edd24f1ccc1fef02
Theo Salzmann <dev@on3iro.de>
2018-03-23 16:33:16 +0100

    wip

--- src/flattenAndMerge.js
+++ src/flattenAndMerge.js
@@ -27,31 +27,28 @@ export const mergeFields = (fields: Fields): Fields => fields.reduce((acc, f, i,
   const node = f.node
 
   const isFirstIteration = i === 0
+  const next = i + 1
   const tail = isFirstIteration
-    ? arr.slice(i + 1)
-    : acc.slice(i + 1)
+    ? arr.slice(next)
+    : acc.slice(next)
 
   if (!tail.length) return acc
+
+  const duplicates = tail.filter(el => el.node === node)
+  const validators = [
+    ...f.validators,
+    ...duplicates.reduce((vals, el) => {
+      return [ ...vals, ...el.validators ]
+    }, [])
+  ]
 
   const head = isFirstIteration
     ? []
     : acc.slice(0, i)
 
-  const duplicates = tail.filter(el => el.node === node)
-  const validators = [
-    ...f.validators,
-    ...duplicates.reduce((a, el) => {
-      return [ ...a, ...el.validators ]
-    }, [])
-  ]
-
   const rest = tail.filter(el => el.node !== node)
 
-  return [
-    ...head,
-    { node, validators },
-    ...rest
-  ]
+  return [ ...head, { node, validators }, ...rest ]
 }, [])
 
 const flattenAndMerge = (fields: NestedFields) => mergeFields(flattenFields(fields))
